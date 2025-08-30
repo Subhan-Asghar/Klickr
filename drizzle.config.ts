1c5605326becadb07e4f110011e4754a6e8a6d6e
Subhan-Asghar <subhanasghar16@gmail.com>
2025-08-31 00:19:04 +0500

    Drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './src/db/migration',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose :true,
+  strict:true
+  
+  
+});
