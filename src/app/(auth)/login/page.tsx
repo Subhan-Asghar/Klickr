a27d2ced5d15c344e25e461c718eb7437af91126
Subhan-Asghar <subhanasghar16@gmail.com>
2025-09-01 00:07:45 +0500

    Login Page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,101 @@
-import React from 'react'
+"use client"
 
+import React, { useState } from "react"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 const Login = () => {
+  const route =useRouter()
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [disable, setDisable] = useState<boolean>(false)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault() 
+
+    const data = {
+      email,
+      password,
+    }
+
+    try {
+      setDisable(true)
+      await axios.post("/api/auth/login", data).
+      then(()=>route.push('/'))
+    } catch (err) {
+      setDisable(false)
+      toast.error(err.response?.data?.message || "Something went wrong")
+    }
+  }
+
   return (
-    <div>page</div>
+    <div className="flex h-screen justify-center items-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+          Enter your credentials
+          </CardDescription>
+        </CardHeader>
+
+        <form onSubmit={handleSubmit}>
+          <CardContent>
+            <div className="flex flex-col gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+          </CardContent>
+
+          <CardFooter className="flex flex-col gap-4">
+            <Button disabled={disable} type="submit" className={"w-full mt-4 cursor-pointer"}>
+              Log in
+            </Button>
+            <p className="text-sm text-muted-foreground text-center">
+            New here? Create an account{" "}
+              <Link
+                href="/signup"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Sign up
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
   )
 }
 
