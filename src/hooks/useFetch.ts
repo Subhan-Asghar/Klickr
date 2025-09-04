3765aedf43cb55d7814e02af8e83f839a37b8575
Subhan-Asghar <subhanasghar16@gmail.com>
2025-09-05 01:25:59 +0500

    useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
+export const useFetch=()=>{
+    const [id,setId]=useState<string>("")
+    const func=async()=>{
+        const res=await axios.get(`api/click/${id}`)
+        return res.data
+    }
+    const{data,isLoading,refetch}=useQuery({
+        queryKey:["click",id],
+        queryFn:func,
+        enabled:!!id
+    })
+    return {data,isLoading,refetch,setId}
+}
