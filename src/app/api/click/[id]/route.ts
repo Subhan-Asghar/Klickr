6cd11e29cbc14c08326645fc7bd3e0218d4e0613
Subhan-Asghar <subhanasghar16@gmail.com>
2025-09-20 16:30:04 +0500

    click coutry list and count

--- src/app/api/click/[id]/route.ts
+++ src/app/api/click/[id]/route.ts
@@ -2,17 +2,37 @@ import { NextRequest, NextResponse} from "next/server";
 import { db } from "@/db/db";
 import { click } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { sql } from "drizzle-orm";
+
 
 export async function GET( req: NextRequest,
 { params }: { params: Promise<{ id: string }> }){
     try{
         const {id}= await params
-        const data=await db.select().from(click).where(eq(click.link_id,id))
-        return NextResponse.json({
-        success:true,
-        data:data
-    },{status:200})
-
+        
+        const clicks = await db
+        .select()
+        .from(click)
+        .where(eq(click.link_id, id));
+  
+      const countryStats = await db
+        .select({
+          country: click.country,
+          total: sql<number>`COUNT(*)`,
+        })
+        .from(click)
+        .where(eq(click.link_id, id))
+        .groupBy(click.country);
+  
+      const totalClicks = clicks.length;
+  
+      return NextResponse.json({
+        success: true,
+        data: {
+          totalClicks,
+          countryStats,
+        },
+      });
     }catch(err){
     
         return NextResponse.json({
